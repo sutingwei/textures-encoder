af869bffcc835bd3ca6e3d6cda15a9e3f0f89d93
jonsu <jonsu@tencent.com>
2022-06-01 19:35:46 +0800

    update

--- textures_encoder.cpp
+++ textures_encoder.cpp
@@ -2,21 +2,28 @@
 #include "stb_image.h"
 
 #include "bx/commandline.h"
+#include "bx/os.h"
+#include "bx/filepath.h"
+#include "bx/string.h"
 
-#include "EtcSourceImage.h"
-#include "EtcImage.h"
-#include "EtcFile.h"
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+bx::DefaultAllocator crt;
+bx::AllocatorI* g_allocator = &crt;
+typedef bx::StringT<&g_allocator> bxString;
 
 void help()
 {
     static const char *help =
             R"(
 Basic usage:
-            --type etc2 --in file.in --out file.out.pkm
-            --type astc --in file.in --out file.out.astc --block [4x4|6x6|8x8]
+            etc2 (etc2 commands)
+            astc (astc commands)
 Example:
-            --type etc2 --in Lenna.png --out Lenna.pkm
-            --type astc --in Lenna.png --out Lenna.astc --block 4x4
+           etc2 ..\Lenna.png -format RGB8 -output ..\Lenna.pkm -verbose
+           astc -cl ..\Lenna.png ..\Lenna.astc 6x6 -medium
 )";
     printf("%s", help);
 }
@@ -24,65 +31,46 @@ Example:
 int main(int argc, const char * argv[])
 {
     auto* c = new bx::CommandLine(argc, argv);
-    if (c->getNum() < 2) {
-        help();
-        exit(1);
-    }
-    const char* compressType = c->findOption("type");
-    const char* inputFile = c->findOption("in");
-    const char* outputFile = c->findOption("out");
-
-    if (inputFile == NULL || outputFile == NULL)
+    if (argc < 2)
     {
-        printf("need input/output file. \n");
         help();
         exit(1);
     }
 
-    int width, height, channels;
-    stbi_uc* data = stbi_load(inputFile, &width, &height, &channels, 0);
+#ifdef _WIN32
+    setbuf(stdout, 0);
+#endif
 
-    if (data == NULL)
-    {
-        printf("image data load failed. require absolute path.\n");
-        exit(1);
-    }
-    stbi_image_free(data);
+    const char* compressType = c->get(1);;
+    auto* executorPath = new bx::FilePath(c->get(0));
+    bxString cwd = bxString(executorPath->getPath());
 
+    char buffer[512];
     if (strcmp(compressType, "etc2") == 0)
     {
-        Etc::Image::Format format = channels == 4 ? Etc::Image::Format::RGBA8 : Etc::Image::Format::RGB8;
-
-        printf("Encoding:\n");
-        printf("  effort = %.f%%\n", ETCCOMP_DEFAULT_EFFORT_LEVEL);
-        printf("  encoding =  %s\n", Etc::Image::EncodingFormatToString(format));
-        printf("  error metric: %s\n", ErrorMetricToString(Etc::ErrorMetric::BT709));
-
-        Etc::SourceImage sourceImage(inputFile, -1, -1);
-        Etc::Image image((float *)sourceImage.GetPixels(),
-                         sourceImage.GetWidth(), sourceImage.GetHeight(),
-                         Etc::ErrorMetric::BT709);
-        Etc::Image::EncodingStatus encStatus = Etc::Image::EncodingStatus::SUCCESS;
-        encStatus = image.Encode(format, Etc::ErrorMetric::BT709, ETCCOMP_DEFAULT_EFFORT_LEVEL, 8,1024);
-
-        printf("  encode time = %dms\n", image.GetEncodingTimeMs());
-        printf("EncodedImage: %s\n", outputFile);
-        printf("status bitfield: %u\n", encStatus);
-
-        Etc::File etcFile(outputFile, Etc::File::Format::INFER_FROM_FILE_EXTENSION,
-                          format,
-                          image.GetEncodingBits(), image.GetEncodingBitsBytes(),
-                          image.GetSourceWidth(), image.GetSourceHeight(),
-                          image.GetExtendedWidth(), image.GetExtendedHeight());
-        etcFile.Write();
+        snprintf (buffer, 512, "%s/EtcTool.exe", cwd.getPtr());
     }
     else if (strcmp(compressType, "astc") == 0)
     {
-        const char* blockSize = c->findOption("block", "6x6");
+        snprintf (buffer, 512, "%s/astcenc-native.exe", cwd.getPtr());
     }
     else
     {
         printf("unsupported compress type. \n");
         help();
+        exit(1);
     }
+
+    char** _argv = new char*[2];
+    _argv[0] = buffer;
+    for (int i = 2; i < argc; ++i) {
+        _argv[i-1] = const_cast<char*>(argv[i]);
+    }
+    _argv[argc-1] = nullptr;
+
+    void* handle = bx::exec(_argv);
+
+#ifdef _WIN32
+    WaitForSingleObject(handle, -1);
+#endif
 }
